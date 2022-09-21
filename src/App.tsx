ed3ea8a2d2da36c7033d500bc3c3619d016187f7
huanglin <2762565371@qq.com>
2022-09-21 20:30:48 +0800

    ✨ feat:添加eslint代码校验

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 const App: React.FC = () => {
   return (
-    <h2 style={{textAlign: 'center'}}>NOTEBOOK APP</h2>
+    <h2 style={{ textAlign: 'center' }}>NOTEBOOK APP</h2>
   )
 }
 
-export default App;
+export default App
