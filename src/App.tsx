303a2aeb5d622d31d31166eeda355d15f81e9e93
huanglin <2762565371@qq.com>
2022-09-22 21:19:24 +0800

    🌈 style:主要页面的样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
 import Header from './components/Header'
+import NoteItem from './components/NoteItem'
+
+const AddIcon: React.FC = () => {
+  return (
+    <div className="add-icon">
+      <i className="iconfont icon-bianjishuru"></i>
+    </div>
+  )
+}
+
 const App: React.FC = () => {
   return (
     <>
       <Header />
+      <div className="body-main">
+        <NoteItem />
+        <NoteItem />
+        <NoteItem />
+        <NoteItem />
+        <NoteItem />
+        <NoteItem />
+        <NoteItem />
+        <NoteItem />
+      </div>
+      <AddIcon />
     </>
   )
 }
